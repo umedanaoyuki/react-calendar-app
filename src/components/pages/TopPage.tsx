0d273c43024f5b1e563d84d20c0fd59a912f92fb
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-03-20 02:25:38 +0900

    Topページ作成2

---
+++ src/components/pages/TopPage.tsx
@@ -0,0 +1,17 @@
+export const TopPage = () => {
+  return (
+    <div>
+      <header>
+        <div className="flex justify-between">
+          <p>スケジュール管理APP</p>
+          <nav>
+            <ul className="flex gap-5">
+              <li>ご利用方法</li>
+              <li>ログイン</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import "./styles/output.css";
 import "./styles/destyle.css";
+import { TopPage } from "./components/pages/TopPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <TopPage />
   </StrictMode>
 );
