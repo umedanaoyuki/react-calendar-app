5d8c532743c9406626de63d06cf48c8300fe1f91
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-03-20 18:36:04 +0900

    feat: ログイン画面の作成4

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,6 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import { PrimaryBtn } from "../atoms/PrimaryBtn";
 
 export const TopPage = () => {
+  const naviagte = useNavigate();
+
   return (
     <div className="text-center">
       <h1 className="text-7xl logo">スケジュール管理APP</h1>
@@ -8,7 +11,7 @@ export const TopPage = () => {
         お互いのスケジュールを管理するアプリです
       </p>
       <div className="pt-[20vh]">
-        <PrimaryBtn>ログイン</PrimaryBtn>
+        <PrimaryBtn onClick={() => naviagte("/login")}>ログイン</PrimaryBtn>
       </div>
     </div>
   );
