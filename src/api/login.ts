5d0b586adf0fe561b110bd5ac00aaca705c13da6
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-03-20 20:53:36 +0900

    feat: ログイン機能の作成

---
+++ src/api/login.ts
@@ -0,0 +1,13 @@
+import { LoginInfoType, LoginReturnType } from "../types/login";
+
+export const login = (info: LoginInfoType): LoginReturnType => {
+  const { email, password } = info;
+  if (email === "test@example.com" && password === "password") {
+    return {
+      id: 1,
+      name: "sample太郎",
+    };
+  } else {
+    throw new Error();
+  }
+};
