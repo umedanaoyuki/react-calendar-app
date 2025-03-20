90dbe1cd4cf822eebda2b957a57ed29d04e47c7e
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-03-20 14:20:04 +0900

    feat: Topページの作成4

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,7 +1,7 @@
 export const TopPage = () => {
   return (
     <div className="relative">
-      <header className="leading-[50px] fixed top-0 left-0 right-0">
+      <header className="bg-white leading-[50px] fixed top-0 left-0 right-0">
         <div className="container mx-auto flex justify-between">
           <p className="logo">スケジュール管理APP</p>
           <nav>
@@ -12,6 +12,19 @@ export const TopPage = () => {
           </nav>
         </div>
       </header>
+      <main className="pt-[50px] bg-gradient-to-r from-lime-100 to-lime-200 h-screen flex flex-col justify-center items-center">
+        <div className="text-center">
+          <h1 className="text-7xl logo">スケジュール管理APP</h1>
+          <p className="pt-[10vh] text-5xl">
+            お互いのスケジュールを管理するアプリです
+          </p>
+          <div className="pt-[20vh]">
+            <button className="bg-lime-800 text-white p-4 text-lg rounded-lg">
+              ログイン
+            </button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
