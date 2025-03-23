fdcd07afe9bc7386b8f72d266268bfa266f3ffaf
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-03-23 23:58:28 +0900

    feat: 1ヶ月カレンダーの作成3

--- src/components/pages/CalendarPage.tsx
+++ src/components/pages/CalendarPage.tsx
@@ -11,6 +11,8 @@ import {
 } from "date-fns";
 import { DAYS_LIST } from "../../constants/calendar";
 import { useEffect, useState } from "react";
+import { CalendarHeader } from "../organisms/CalendarHeader";
+import { CalenderBody } from "../organisms/CalenderBody";
 
 export const CalendarPage = () => {
   const today = new Date();
@@ -44,36 +46,8 @@ export const CalendarPage = () => {
     <>
       <h1 className=" font-bold text-3xl mb-5">{`${getMonth(today) + 1}月`}</h1>
       <table className="w-[80%] border-collapse border-2 border-solid border-lime-800 table-fixed">
-        <thead>
-          <tr className="bg-lime-800 text-white rounded-tl-lg rounded-tr-lg py-10">
-            {DAYS_LIST.map((day) => (
-              <th key={day} className="text-center text-xl py-3">
-                {day}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {dateList.map((oneWeek) => (
-            <tr key={`week-${getDate(oneWeek[0])}`} className="mx-10">
-              {oneWeek.map((item) => (
-                <td
-                  key={`day-${getDate(oneWeek[0])}`}
-                  className="bg-white h-[10vh] border-2 border-solid border-lime-800"
-                >
-                  <span
-                    className={`inline-block w-[20px] leading-[20px] text-center ${dateColor(
-                      item,
-                      today
-                    )}`}
-                  >
-                    {getDate(item)}
-                  </span>
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
+        <CalendarHeader />
+        <CalenderBody currentDate={today} dateList={dateList} />
       </table>
     </>
   );
